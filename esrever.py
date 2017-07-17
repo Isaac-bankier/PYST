14ececd03ae93f9f96e82f38787123eee3a22acb
isaac bankier <isaacbankier@gmail.com>
2017-07-17 23:02:07 +1000

    .resrever gnirts eht detelpmoC

---
+++ esrever.py
@@ -0,0 +1,10 @@
+##############################
+#         esrever.py         #
+#      gnirts a esreveR      #
+#      reiknaB caasI yB      #
+##############################
+
+def reverse(string):
+    return string[::-1] #This line says that you should included the entire string but put each character backwards.
+
+print(reverse(input("esrever ot tnaw uoy od gnirts tahW: ")))
