ef36f6c81b780b97b7c6500e95b69007f36c44f1
isaac bankier <isaacbankier@gmail.com>
2017-07-17 22:36:56 +1000

    Finished the histogram.

---
+++ histogram.py
@@ -0,0 +1,11 @@
+##############################
+#        histogram.py        #
+#    generate a histogram    #
+#      By Isaac Bankier      #
+##############################
+
+def histogram(dataSet, symbol="*", gap=0):
+    for data in dataSet: # Go through each entry in the list
+        print(symbol*data+gap*"\n") # Print it out
+
+histogram([4, 9, 7])
