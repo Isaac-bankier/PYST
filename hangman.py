d4678ac5f5f72bb6f1e96b33567ec3ecbd36dc49
isaac bankier <isaacbankier@gmail.com>
2017-08-29 18:46:33 +1000

    Did hangman.py

---
+++ hangman.py
@@ -0,0 +1,79 @@
+##############################
+#        guess_word.py       #
+#       Guess the word       #
+#      By Isaac Bankier      #
+##############################
+
+import random
+
+class game(object):
+    def __init__(self, words=["linux", "onion", "github", "firefox", "gimp", "kernal"], guesses=7):
+        self.words = words
+        self.maxGuesses = guesses
+        self.word = ""
+        self.guesses = 0
+        self.art =\
+["","""
+    ___
+""","""
+       |
+       |
+       |
+    ___|
+""","""
+________
+       |
+       |
+       |
+    ___|
+""","""
+________
+      \|
+       |
+       |
+    ___|
+""","""
+________
+|     \|
+       |
+       |
+    ___|
+""","""
+________
+|     \|
+O      |
+       |
+    ___|
+""","""
+________
+|     \|
+O      |
+X      |
+    ___|
+"""]
+
+    def start(self):
+        self.word = list(random.choice(self.words))
+        return self.word
+
+    def canPlay(self):
+        return (self.guesses < self.maxGuesses) and (not (len(self.word) == 0))
+
+    def guess(self, char):
+        try:
+            self.word.remove(char)
+            if len(self.word) == 0:
+                return "You win!"
+        except ValueError:
+            self.guesses += 1
+            if self.guesses == self.maxGuesses:
+                return self.art[self.guesses]+"\nGame over!"
+            return self.art[self.guesses]+"You have " + str(self.maxGuesses - self.guesses) + " guesses left. "
+
+
+        return "Correct. You have " + str(self.maxGuesses - self.guesses) + " guesses left. "
+
+g = game()
+print(g.start())
+while g.canPlay():
+    print(g.guess(input("Guess: ")[0]))
